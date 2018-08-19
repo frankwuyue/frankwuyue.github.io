33e7fc18a30e3d95d432f71c4c06cac832869a14
frankwuyue <wuyuefrank@gmail.com>
2018-08-20 00:17:36 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-html="html"></div>
+  </div>
+</template>
+
+<script>
+const markdown = require('../static/markdown/test.md');
+
+export default class extends Vue {
+  get html() {
+    return markdown;
+  }
+}
+</script>
