ee61171be0f4c14088c6d114d7d0ba0b045a8b13
frankwuyue <wuyuefrank@gmail.com>
2019-02-02 23:47:50 +0900

    fix mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,6 @@ const host =
   process.env.npm_package_config_nuxt_host ||
   'localhost';
 module.exports = {
-  mode: 'spa',
   env: {
     baseUrl: process.env.BASE_URL || `http://${host}:${port}`
   },
