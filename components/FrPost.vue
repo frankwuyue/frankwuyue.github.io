e20ebf0bbfb717069e6bc2eece7b08ecb93bea79
frankwuyue <wuyuefrank@gmail.com>
2019-02-02 20:17:49 +0900

    fix route of about.md

--- components/FrPost.vue
+++ components/FrPost.vue
@@ -7,7 +7,7 @@
 
 <script lang='ts'>
 import { Component, Vue } from 'nuxt-property-decorator';
-const markdown = require('~/content/posts/about.md');
+const markdown = require('~/content/about.md');
 @Component
 export default class FrPost extends Vue {
   name: "fr-post";
---
+++ content/about.md
@@ -0,0 +1,6 @@
+# About Me
+
+- Name: Wu Yue
+- EnglishName: Frank
+- Location: Tokyo
+- Hobby: Programming, Cooking, Traveling
